f05eb906ca86d876dd0cef3bdff24d736b155df3
indunil-jay <indunil.jayz@gmail.com>
2024-08-05 04:58:30 +0530

    add tour resouce CRUD routes and prepare for functioning

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 
+import tourRouter from "./routes/tour-routes";
+
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("testing end point.");
-});
+app.use(express.json());
+
+app.use("/api/v1/tours", tourRouter);
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`server is listing t0 ${PORT}`);
 });
---
+++ api/controllers/tour-controllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const getAllTours = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "success" });
+};
+export const getTour = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "success" });
+};
+export const createTour = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "success" });
+};
+export const updateTour = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "success" });
+};
+export const deleteTour = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "success" });
+};
