746e020fa8b11f9155ff7b094ed217600fbdcfd0
indunil-jay <indunil.jayz@gmail.com>
2024-08-05 05:47:51 +0530

    add dotenv cross-env morgon packages and refactor code

--- api/app.ts
+++ api/app.ts
@@ -1,15 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 import tourRouter from "./routes/tour-routes";
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+//middlewares
 app.use(express.json());
 
+//Resource Routes
 app.use("/api/v1/tours", tourRouter);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`server is listing t0 ${PORT}`);
-});
+export default app;
