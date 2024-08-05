54bab163aab1151aacba9fbaa569bf5d06da5b36
indunil-jay <indunil.jayz@gmail.com>
2024-08-05 10:52:52 +0530

    add tour schema and implement routes

--- api/controllers/tour-controllers.ts
+++ api/controllers/tour-controllers.ts
@@ -1,17 +1,95 @@
 import { Request, Response } from "express";
+import Tour from "../models/tour-model";
 
-export const getAllTours = (req: Request, res: Response) => {
-  return res.status(200).json({ message: "success" });
+export const createTour = async (req: Request, res: Response) => {
+  try {
+    const tour = await Tour.create(req.body);
+
+    return res.status(201).json({
+      status: "success",
+      message: "New tour created successfully.",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
-export const getTour = (req: Request, res: Response) => {
-  return res.status(200).json({ message: "success" });
+
+export const getAllTours = async (req: Request, res: Response) => {
+  try {
+    const tours = await Tour.find();
+
+    return res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
-export const createTour = (req: Request, res: Response) => {
-  return res.status(200).json({ message: "success" });
+
+export const getTour = async (req: Request, res: Response) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
-export const updateTour = (req: Request, res: Response) => {
-  return res.status(200).json({ message: "success" });
+
+export const updateTour = async (req: Request, res: Response) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
-export const deleteTour = (req: Request, res: Response) => {
-  return res.status(200).json({ message: "success" });
+
+export const deleteTour = async (req: Request, res: Response) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
---
+++ api/models/tour-model.ts
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+
+interface ITour {
+  name: string;
+  price: number;
+  duration: number;
+  maxGroupSize: number;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  difficulty: "medium" | "easy" | "difficult";
+  priceDiscount: Number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: [Date];
+  // need to add below two becuase of  {timestamps: true}
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const tourSchema = new mongoose.Schema<ITour>(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name."],
+      unique: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price."],
+    },
+
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration."],
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size."],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty."],
+    },
+
+    priceDiscount: Number,
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a summary."],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description."],
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image."],
+    },
+
+    images: [String],
+
+    startDates: [Date],
+
+    //TODO: locations details
+  },
+
+  { timestamps: true }
+);
+
+const Tour = mongoose.model<ITour>("Tour", tourSchema);
+
+export default Tour;
