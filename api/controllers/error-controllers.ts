d485c8ddbb93094a86ee7d16f0a471daa9c46d0a
indunil-jay <indunil.jayz@gmail.com>
2024-08-06 07:20:10 +0530

    categorize enviorments and send different errors

--- api/controllers/error-controllers.ts
+++ api/controllers/error-controllers.ts
@@ -1,6 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/app-error";
 
+const sendErrorToDevelopmentEnviroment = (error: AppError, res: Response) => {
+  return res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error: error,
+  });
+};
+
+const sendErrorToProductionEnviroment = (error: AppError, res: Response) => {
+  //operational error that need to be know the client
+  if (error.isOperational) {
+    return res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+  //programming error or bugs that don't leak to the client (send generic message)
+  else {
+    return res.status(500).json({
+      status: "error",
+      message: "something went wrong!",
+    });
+  }
+};
+
 const globalErrorHandler = (
   error: AppError,
   req: Request,
@@ -10,10 +36,12 @@ const globalErrorHandler = (
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
 
-  return res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorToDevelopmentEnviroment(error, res);
+  }
+  if (process.env.NODE_ENV === "production") {
+    sendErrorToProductionEnviroment(error, res);
+  }
 };
 
 export default globalErrorHandler;
